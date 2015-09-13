97923dfc5b61171b781a0131603f643bdf0f72e1
steve <stevie@steviesmith.com>
2015-09-13 09:14:25 +0100

    Tidied some layout and css. Removed redundant things from the bundling output and gulp tasks.

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -3,9 +3,43 @@ var runSequence = require('run-sequence');
 var vinylPaths = require('vinyl-paths');
 var paths = require('../paths');
 var del = require('del');
-var expect = require('gulp-expect-file');
 var bundle = require('aurelia-bundler').bundle;
 var config;
+
+config = {
+  force: true,
+  packagePath: './tmp',
+  bundles: {
+    "app-bundle": {
+      includes: [
+        'src/**/*',
+        'src/**/*.html!text',
+        'src/**/*.css!text'
+      ],
+      options: {
+        inject: true,
+        minify: false
+      }
+    },
+    "aurelia": {
+      includes: [
+        'aurelia-bootstrapper',
+        'aurelia-router',
+        'github:aurelia/templating-binding',
+        'github:aurelia/templating-resources',
+        'github:aurelia/templating-router',
+        'github:aurelia/loader-default',
+        'github:aurelia/history-browser',
+        'github:aurelia/logging-console',
+        'github:aurelia/validation@0.3.0/resources/*'
+      ],
+      options: {
+        inject: true,
+        minify: false
+      }
+    }
+  }
+};
 
 gulp.task('copy-aurelia-build-files', function () {
   return gulp.src([paths.top + 'package.json'])
@@ -25,17 +59,11 @@ gulp.task('copy-deploy-jspm', function () {
 
 gulp.task('copy-deploy-styles', function () {
   return gulp.src([
-    paths.tmp + 'app/styles/*'
+    paths.tmp + 'app/styles/*.*'
   ]).pipe(gulp.dest(paths.deploy + 'styles'));
 });
 
-gulp.task('copy-deploy-toolkit', function () {
-  return gulp.src([
-    paths.tmp + 'app/toolkit/**/*'
-  ]).pipe(gulp.dest(paths.deploy + 'toolkit'));
-});
-
-gulp.task('build-deploy', ['copy-deploy-styles', 'copy-deploy-jspm', 'copy-deploy-toolkit'], function () {
+gulp.task('build-deploy', ['copy-deploy-styles', 'copy-deploy-jspm'], function () {
   return gulp.src([
     paths.tmp + 'app/index.html',
     paths.tmp + 'app/*.js'
@@ -51,41 +79,6 @@ gulp.task('clean-tmp', function () {
   return gulp.src([paths.tmp])
     .pipe(vinylPaths(del));
 });
-
-config = {
-  force: true,
-  packagePath: './tmp',
-  bundles: {
-    "app-bundle": {
-      includes: [
-        'src/**/*',
-        'src/**/*.html!text',
-        'src/**/*.css!text'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    },
-    "aurelia": {
-      includes: [
-        'aurelia-bootstrapper',
-        'aurelia-router',
-        'github:aurelia/templating-binding',
-        'github:aurelia/templating-resources',
-        'github:aurelia/templating-router',
-        'github:aurelia/loader-default',
-        'github:aurelia/history-browser',
-        'github:aurelia/logging-console',
-        'github:aurelia/validation@0.3.0/resources/*'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    }
-  }
-};
 
 gulp.task('run-bundler', function () {
   return bundle(config);

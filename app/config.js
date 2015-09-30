3d1cee44a0c332b0264e1147075a276ff72ad992
stevies <stevie@steviesmith.com>
2015-09-30 07:39:50 +0100

    Updated to validation 0.3.1

--- app/config.js
+++ app/config.js
@@ -36,10 +36,10 @@ System.config({
     "aurelia-templating-binding": "github:aurelia/templating-binding@0.15.0",
     "aurelia-templating-resources": "github:aurelia/templating-resources@0.15.2",
     "aurelia-templating-router": "github:aurelia/templating-router@0.16.1",
-    "aurelia-validation": "github:aurelia/validation@0.3.0",
-    "babel": "npm:babel-core@5.8.24",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
-    "core-js": "npm:core-js@1.1.4",
+    "aurelia-validation": "github:aurelia/validation@0.3.1",
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.2.0",
     "lodash": "npm:lodash@3.10.1",
     "moment": "github:moment/moment@2.10.6",
     "text": "github:systemjs/plugin-text@0.0.2",
@@ -98,7 +98,7 @@ System.config({
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.3.0",
       "aurelia-metadata": "github:aurelia/metadata@0.8.0",
       "aurelia-path": "github:aurelia/path@0.9.0",
-      "core-js": "npm:core-js@0.9.18"
+      "core-js": "github:zloirock/core-js@0.8.4"
     },
     "github:aurelia/logging-console@0.7.1": {
       "aurelia-logging": "github:aurelia/logging@0.7.0"
@@ -152,7 +152,7 @@ System.config({
       "aurelia-task-queue": "github:aurelia/task-queue@0.7.0",
       "core-js": "npm:core-js@0.9.18"
     },
-    "github:aurelia/validation@0.3.0": {
+    "github:aurelia/validation@0.3.1": {
       "aurelia-binding": "github:aurelia/binding@0.9.1",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.10.1",
       "aurelia-logging": "github:aurelia/logging@0.7.0",
@@ -162,7 +162,7 @@ System.config({
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.8.20": {
+    "npm:babel-runtime@5.8.25": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
@@ -170,7 +170,7 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:core-js@1.1.4": {
+    "npm:core-js@1.2.0": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
